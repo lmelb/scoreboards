34f783792d39b6beea94e1999ca15504bff9128c
lars <lars@melbye.de>
2025-03-09 16:11:22 +0100

    add class prop to tally

--- src/lib/components/closeable-badge/tally.svelte
+++ src/lib/components/closeable-badge/tally.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
 	import { Tally1, Tally2, Tally3, Tally4, Tally5 } from 'lucide-svelte';
 
-	const { value } = $props();
+	const { value, class: className }: { value: number; class: string } = $props();
 </script>
 
 {#if value === 1}
-	<Tally1 aria-label="1" />
+	<Tally1 class={className} aria-label="1" />
 {:else if value === 2}
-	<Tally2 aria-label="2" />
+	<Tally2 class={className} aria-label="2" />
 {:else if value === 3}
-	<Tally3 aria-label="3" />
+	<Tally3 class={className} aria-label="3" />
 {:else if value === 4}
-	<Tally4 class="text-red-800" aria-label="4" />
+	<Tally4 class={cn('text-red-800', className)} aria-label="4" />
 {:else if value === 5}
-	<Tally5 class="text-red-500" aria-label="5" />
+	<Tally5 class={cn('text-red-500', className)} aria-label="5" />
 {/if}
