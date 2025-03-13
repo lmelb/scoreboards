3f3790aba5e621fc1cb25de50844f430bd33bf5f
lmelb <lars@melbye.de>
2025-03-13 20:06:29 +0100

    fix playerName validation

--- src/routes/players/add-player-form.svelte
+++ src/routes/players/add-player-form.svelte
@@ -3,7 +3,7 @@
 	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
 
-	import { Player } from '$lib/models/Player';
+	import { Player } from '$lib/models/player';
 	import { UserPlus } from 'lucide-svelte';
 	import { validatePlayerName } from './player-name-validation';
 	import { getPlayersService } from '$lib/services/player.service.svelte';
@@ -12,13 +12,14 @@
 
 	let playerNameInput = $state<HTMLInputElement>()!;
 
-	type Validation = { valid: boolean; errors: string[] };
+	type Validation = { valid: boolean; playerName?: string; errors: string[] };
 	let validation: Validation = $state({ valid: false, errors: [] });
 
 	function onchange() {
 		const validatedPlayer = validatePlayerName(playerNameInput.value, playerService);
 		validation = {
 			valid: validatedPlayer.success,
+			playerName: validatedPlayer.data,
 			errors: validatedPlayer.error?.issues.map((error) => error.message) ?? []
 		};
 	}
@@ -27,7 +28,7 @@
 		event.preventDefault();
 		if (!validation.valid) return;
 
-		const player = new Player(playerNameInput.value);
+		const player = new Player(validation.playerName!);
 		playerService.add(player);
 
 		playerNameInput.value = '';
